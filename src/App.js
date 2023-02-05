6a379fc2bf2597ad945def588bdcc7a4b06715ad
manhtieneaut <20200892@eaut.edu.vn>
2023-02-05 19:07:29 +0700

    bài thực hành 4

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './App.css';
+
+
+function App() {
+
+  const [userInput, setUserInput] = useState("");
+  const handleInputChange = (e) => {
+    setUserInput(e.target.value);
+  };
+  const handleButtonClick = (e) => {
+    alert(userInput)
+  }
+  return (
+    <div>
+      <input
+        type="text"
+        name="user"
+        placeholder="Please type somthing"
+        onChange={handleInputChange}
+      />
+      <button
+        onClick={handleButtonClick}
+      >Print</button>
+    </div>
+  );
+}
+
+
+
+
+export default App;
